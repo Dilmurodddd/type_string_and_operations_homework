877199dbb3b1d7bf0777acb5c33d37435d0debd2
ali <mamarajabovdddd@gmail.com>
2023-04-08 19:25:01 +0500

    Bajardim

--- String01.py
+++ String01.py
@@ -5,4 +5,5 @@ def main():
     Returns:
         str: return answer.
     """
-    return 
+    return '1\n2\n3\n4\n5\n6\n7\n8'
+print(main())
--- String02.py
+++ String02.py
@@ -1,4 +1,4 @@
-def main():
+def main(a):
     """
     Return a string consisting only of uppercase letters.
     Args:
@@ -6,4 +6,7 @@ def main():
     Returns:
         str: return answer.
     """
-    return 
+    
+   
+    return str(a).upper()
+print(main('abcd'))
